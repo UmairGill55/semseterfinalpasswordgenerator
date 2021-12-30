8dcee6c896a7403f205f1f608a6369261fc4884c
UmairGill55 <91935137+UmairGill55@users.noreply.github.com>
2021-12-31 01:37:38 +0500

    u

--- lib/easyview.dart
+++ lib/easyview.dart
@@ -10,6 +10,180 @@ class ReportsView extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Reports'),
         centerTitle: true,
+	import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:password_generator/theme.dart';
+import 'package:clipboard/clipboard.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var isCheckedSpecial = true;
+  var isCheckedNumbers = true;
+  var isCheckedUppercase = true;
+  var passwordLength = 16;
+  var password = '';
+  TextEditingController passwordLengthForm = TextEditingController();
+  void generatePassword() {
+    var chars = 'abcdefghijklmnopqrstuvwxyz';
+    passwordLength = int.parse(passwordLengthForm.text);
+    if (isCheckedSpecial) {
+      chars += '!@#%^&*()';
+    }
+    if (isCheckedNumbers) {
+      chars += '01234567890123456789';
+    }
+    if (isCheckedUppercase) {
+      chars += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    }
+    var r = Random();
+    password =
+        List.generate(passwordLength, (index) => chars[r.nextInt(chars.length)])
+            .join();
+    setState(() {});
+  }
+
+  void copyPassword() {
+    var passwordString = password.toString();
+    FlutterClipboard.copy(passwordString);
+    print(passwordString);
+  }
+
+  @override
+  void initState() {
+    passwordLengthForm.text = '16';
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          backgroundColor: const Color(0xFF000000),
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Password',
+                style: Theme.of(context).textTheme.headline6,
+              ),
+              Text(
+                ' Generator',
+                style: Theme.of(context).textTheme.subtitle1,
+              ),
+            ],
+          )),
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Column(
+              children: [
+                Text(
+                  password,
+                  softWrap: true,
+                  textAlign: TextAlign.center,
+                  maxLines: 2,
+                  style: Theme.of(context).textTheme.headline4,
+                )
+              ],
+            ),
+            const Spacer(),
+            Column(
+              children: [
+                const Text('Password length'),
+                Container(
+                  width: 40,
+                  child: TextField(
+                    controller: passwordLengthForm,
+                    keyboardType: TextInputType.number,
+                    maxLength: 3,
+                    cursorColor: Color(0xFFFF40B3),
+                    textAlign: TextAlign.center,
+                    autofocus: false,
+                    decoration: const InputDecoration(
+                      counterText: "",
+                      enabledBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey),
+                      ),
+                      focusedBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: Color(0xFFFF40B3)),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                const Text('Special characters'),
+                Checkbox(
+                  value: isCheckedSpecial,
+                  onChanged: (value) {
+                    setState(() {
+                      isCheckedSpecial = value!;
+                    });
+                  },
+                  fillColor: checkboxColor,
+                ),
+                const Text('Uppercase letters'),
+                Checkbox(
+                  value: isCheckedUppercase,
+                  onChanged: (value) {
+                    setState(() {
+                      isCheckedUppercase = value!;
+                    });
+                  },
+                  fillColor: checkboxColor,
+                ),
+                const Text('Numbers'),
+                Checkbox(
+                  value: isCheckedNumbers,
+                  onChanged: (value) {
+                    setState(() {
+                      isCheckedNumbers = value!;
+                    });
+                  },
+                  fillColor: checkboxColor,
+                ),
+              ],
+            ),
+            const Spacer(),
+            Column(
+              children: [
+                TextButton(
+                  onPressed: generatePassword,
+                  style: secondaryButtonStyle,
+                  child: Text(
+                    'Generate password',
+                    style: Theme.of(context).textTheme.headline6,
+                  ),
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                TextButton(
+                  onPressed: copyPassword,
+                  style: primaryButtonStyle,
+                  child: Text(
+                    '    Copy password    ',
+                    style: Theme.of(context).textTheme.headline5,
+                  ),
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
       ),
     ));
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,226 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'routes.dart';
-import 'storageview.dart';
-import 'harderview.dart';
-import 'easyview.dart';
 
+var _char = false;
+var _num = false;
+var _lower = false;
+var _upper = false;
+var _len = 11.0;
+var length;
 void main() {
-  runApp(MyApp());
+  runApp(
+    MaterialApp(
+      home: PassWordGen(),
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget {
+class PassWordGen extends StatefulWidget {
+  @override
+  _PassWordGenState createState() => _PassWordGenState();
+}
+
+class _PassWordGenState extends State<PassWordGen> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Password Generator',
-      theme: _createTheme(),
-      home: TransactionView(),
-      routes: {
-        routes.transaction: (context) => TransactionView(),
-        routes.categories: (context) => CategoriesView(),
-        routes.reports: (context) => ReportsView(),
-      },
+    return Scaffold(
+      backgroundColor: Colors.grey[900],
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.redAccent,
+        title: Text(
+          'Password Generator',
+        ),
+      ),
+      body: ListView(
+        children: <Widget>[
+          SizedBox(
+            height: 10,
+          ),
+          CheckboxListTile(
+            value: _lower,
+            controlAffinity: ListTileControlAffinity.leading,
+            title: Text(
+              'Lowercase',
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+            secondary: Icon(
+              CupertinoIcons.info,
+              color: Colors.white,
+            ),
+            onChanged: (bool value) {
+              setState(() {
+                _lower = value;
+              });
+            },
+            activeColor: Colors.redAccent,
+            checkColor: Colors.white,
+          ),
+          CheckboxListTile(
+            value: _upper,
+            controlAffinity: ListTileControlAffinity.leading,
+            title: Text(
+              'UpperCase',
+              style: TextStyle(color: Colors.white),
+            ),
+            secondary: Icon(
+              CupertinoIcons.italic,
+              color: Colors.white,
+            ),
+            onChanged: (bool value) {
+              setState(() {
+                _upper = value;
+              });
+            },
+            activeColor: Colors.redAccent,
+          ),
+          CheckboxListTile(
+            title: Text(
+              'Special Characters',
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+            secondary: Icon(
+              CupertinoIcons.money_dollar,
+              color: Colors.white,
+            ),
+            value: _char,
+            controlAffinity: ListTileControlAffinity.leading,
+            onChanged: (bool value) {
+              setState(() {
+                _char = value;
+              });
+            },
+            activeColor: Colors.redAccent,
+          ),
+          CheckboxListTile(
+            value: _num,
+            controlAffinity: ListTileControlAffinity.leading,
+            title: Text(
+              'Numbers',
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+            secondary: Icon(
+              CupertinoIcons.number_circle,
+              color: Colors.white,
+            ),
+            onChanged: (bool value) {
+              setState(() {
+                _num = value;
+              });
+            },
+            activeColor: Colors.redAccent,
+          ),
+          ListTile(
+            title: Text(
+              'Length',
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+          Slider(
+            value: _len,
+            min: 8,
+            max: 25,
+            label: _len.round().toString(),
+            divisions: 5,
+            onChanged: (double value) {
+              setState(() {
+                _len = value;
+                length = value.round;
+              });
+            },
+            activeColor: Colors.redAccent,
+            inactiveColor: Colors.grey[900],
+          ),
+          Center(
+            child: RaisedButton(
+              onPressed: () {
+                showDialog(
+                  context: context,
+                  builder: (BuildContext context) => _buildPopupDialog(context),
+                );
+              },
+              color: Colors.redAccent,
+              child: Text(
+                'Generate',
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
 
-ThemeData _createTheme() {
-  return ThemeData(
-    brightness: Brightness.light,
-    primaryColor: Colors.purple[800],
-    accentColor: Colors.pink[600],
-    canvasColor: Color.fromRGBO(251, 228, 251, 1),
-    fontFamily: 'Georgia',
-  );
+Widget _buildPopupDialog(BuildContext context) {
+  if (_char == false && _lower == false && _upper == false && _num == false) {
+    return AlertDialog(
+      backgroundColor: Colors.grey[900],
+      title: Text(
+        'Error',
+        style: TextStyle(
+          color: Colors.white,
+        ),
+      ),
+      content: Text(
+        'Select at least one option.',
+        style: TextStyle(
+          color: Colors.white,
+        ),
+      ),
+      actions: <Widget>[
+        FlatButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          child: Text(
+            'Ok',
+            style: TextStyle(color: Colors.redAccent),
+          ),
+        )
+      ],
+    );
+  } else {
+    return AlertDialog(
+      backgroundColor: Colors.grey[900],
+      title: Text(
+        'Your Password',
+        style: TextStyle(
+          color: Colors.white,
+        ),
+      ),
+      content: Text(
+        'Password',
+        style: TextStyle(
+          color: Colors.white,
+        ),
+      ),
+      actions: <Widget>[
+        FlatButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          child: Text(
+            'Ok',
+            style: TextStyle(color: Colors.redAccent),
+          ),
+        ),
+        FlatButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          child: Text(
+            'Copy to Clipboard',
+            style: TextStyle(color: Colors.redAccent),
+          ),
+        ),
+      ],
+    );
+  }
 }
