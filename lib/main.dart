aab060885f3d2af8a5605f4ff543ec8ae4a1f60d
UmairGill55 <91935137+UmairGill55@users.noreply.github.com>
2021-12-31 00:57:04 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Navigation Drawer Demo',
+      title: 'Password Generator',
       theme: _createTheme(),
       home: TransactionView(),
       routes: {
